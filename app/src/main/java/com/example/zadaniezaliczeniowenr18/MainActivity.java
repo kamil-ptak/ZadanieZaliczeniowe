2f0e1190ffb5c4536b1e735dad87d6c4540821bb
Kamil Ptak <kamil-ptak@gmail.com>
2025-12-05 13:18:28 +0100

    pobranie id elementow z activity_main.xml do mainactivity.java

--- app/src/main/java/com/example/zadaniezaliczeniowenr18/MainActivity.java
+++ app/src/main/java/com/example/zadaniezaliczeniowenr18/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.zadaniezaliczeniowenr18;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText etFirstName;
+    private EditText etLastName;
+    private Spinner spinner;
+    private EditText etPasswordLength;
+    private CheckBox cbUpperLower;
+    private CheckBox cbDigits;
+    private CheckBox cbSpecial;
+    private Button btnGenerate;
+    private Button btnConfirm;
+
+    private String generatedPassword = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        etFirstName = findViewById(R.id.etFirstName);
+        etLastName = findViewById(R.id.etLastName);
+        spinner = findViewById(R.id.spinner);
+        etPasswordLength = findViewById(R.id.etPasswordLength);
+        cbUpperLower = findViewById(R.id.cbUpperLower);
+        cbDigits = findViewById(R.id.cbDigits);
+        cbSpecial = findViewById(R.id.cbSpecial);
+        btnGenerate = findViewById(R.id.btnGenerate);
+        btnConfirm = findViewById(R.id.btnConfirm);
+
+
     }
 }
